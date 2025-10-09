65f728959ef08b2d6794e5b79fe0c8ca0602e3ac
sqaaq <80844997+sqaaq@users.noreply.github.com>
2025-10-09 12:57:41 +0300

    Create TransferAutomation.meta.js

---
+++ userscripts/TransferAutomation.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Category Automation
+// @namespace    https://github.com/sqaaq/automations/
+// @version      1.3
+// @description  Автоматическое изменение категории и перенос заявки / iOS/Android/Mac/Win
+// @match        https://*.*.*/front/ticket.form.php*
+// @grant        none
+// @author       Egor & AI
+// @downloadURL  https://sqaaq.github.io/automations/userscripts/TransferAutomation.user.js
+// @updateURL    https://sqaaq.github.io/automations/userscripts/TransferAutomation.meta.js
+// ==/UserScript==
