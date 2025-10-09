2ce010ddecfb509aeb61f0f4b9f9cb9468468aa3
sqaaq <80844997+sqaaq@users.noreply.github.com>
2025-10-09 12:56:02 +0300

    Create editADuser.meta.js

---
+++ userscripts/editADuser.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Edit AD user .PS
+// @namespace    https://sqaaq.github.io/automations
+// @version      1.9.3
+// @description  Кнопка появляется только при наличии "Выход нового сотрудника" в заголовке
+// @author       Egor & AI
+// @match        https://*/front/ticket.form.php?*
+// @downloadURL  https://sqaaq.github.com/automations/userscripts/editADuser.user.js
+// @updateURL    https://sqaaq.github.com/automations/userscripts/editADuser.meta.js
+// @grant        none
+// ==/UserScript==
