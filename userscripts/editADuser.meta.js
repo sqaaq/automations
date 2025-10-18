91a1d500a02e0165fd048eebaa9096dad2223d0b
sqaaq <80844997+sqaaq@users.noreply.github.com>
2025-10-18 09:09:34 +0300

    Update editADuser.meta.js
    added autoanswer

--- userscripts/editADuser.meta.js
+++ userscripts/editADuser.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Edit AD user .PS
 // @namespace    https://sqaaq.github.io/automations
-// @version      1.9.4
+// @version      1.9.5
 // @description  Кнопка появляется только при наличии "Выход нового сотрудника" в заголовке
 // @author       Egor & AI
 // @match        https://*/front/ticket.form.php?*
