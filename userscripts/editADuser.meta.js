73b86c7905cb6efc645acab639dabe76757ac99c
sqaaq <80844997+sqaaq@users.noreply.github.com>
2025-11-17 16:24:13 +0300

    Update editADuser.meta.js

--- userscripts/editADuser.meta.js
+++ userscripts/editADuser.meta.js
@@ -5,7 +5,7 @@
 // @description  Кнопка появляется только при наличии "Выход нового сотрудника" в заголовке + FullAutoAnswer + Passw gen + mailbox create
 // @author       Egor & AI
 // @match        https://*/front/ticket.form.php?*
-// @downloadURL  https://sqaaq.github.com/automations/userscripts/editADuser.user.js
-// @updateURL    https://sqaaq.github.com/automations/userscripts/editADuser.meta.js
+// @downloadURL  https://sqaaq.github.io/automations/userscripts/editADuser.user.js
+// @updateURL    https://sqaaq.github.io/automations/userscripts/editADuser.meta.js
 // @grant        none
 // ==/UserScript==
