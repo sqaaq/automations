fb1c3e9937cebc5aae7fe7953b6b5ea946b30943
sqaaq <80844997+sqaaq@users.noreply.github.com>
2025-11-17 16:03:54 +0300

    Update editADuser.meta.js
    added domain filtration

--- userscripts/editADuser.meta.js
+++ userscripts/editADuser.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Edit AD user .PS
 // @namespace    https://sqaaq.github.io/automations
-// @version      1.11
+// @version      1.11.1
 // @description  Кнопка появляется только при наличии "Выход нового сотрудника" в заголовке + FullAutoAnswer + Passw gen + mailbox create
 // @author       Egor & AI
 // @match        https://*/front/ticket.form.php?*
-// @downloadURL  https://sqaaq.github.io/automations/userscripts/editADuser.user.js
-// @updateURL    https://sqaaq.github.io/automations/userscripts/editADuser.meta.js
+// @downloadURL  https://sqaaq.github.com/automations/userscripts/editADuser.user.js
+// @updateURL    https://sqaaq.github.com/automations/userscripts/editADuser.meta.js
 // @grant        none
 // ==/UserScript==
